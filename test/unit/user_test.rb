9983fc8a9832d563dfc209d9fa673eff8d61883b
Martyn Taylor <mtaylor@redhat.com>
2012-11-30 11:59:47 +0000

    Added single associated pass exampel

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "SystemStackError when using accepts_nested_attributes and inverse_of" do
+  test "Everything works fine when setting only 1 associated object" do
+    user = User.new
+    address1 = Address.new
+
+    user.addresses << address1
+    user.save
+  end
+
+  test "SystemStackError when using accepts_nested_attributes and inverse_of when setting >1 association object" do
     user = User.new
     address1 = Address.new
     address2 = Address.new
